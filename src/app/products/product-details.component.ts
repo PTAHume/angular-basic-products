94c2724a93f67eeca93f5243f6480ccd81c78258
PTAHume <patricktahume@gmail.com>
2022-10-18 19:34:12 +0100

    routing part 1

---
+++ src/app/products/product-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
