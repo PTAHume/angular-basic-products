43842842e16c8ef0dd6dd58cb792320994e9f462
PTAHume <patricktahume@gmail.com>
2022-10-18 18:50:58 +0100

    observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,40 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './../../api/products.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import IProduct from './IProduct';
+import { Subscribable, Subscription } from 'rxjs';
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export default class ProductListComponent implements OnInit {
+export default class ProductListComponent implements OnInit, OnDestroy {
   title = 'Product List';
   message = '';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-  ];
+  products: IProduct[] = [];
+  errorMessage: string = '';
+  service!: Subscription;
+
   private _listFilter = '';
+
+  constructor(private productsService: ProductsService) {}
+
   get listFilter(): string {
     return this._listFilter;
   }
@@ -42,6 +29,20 @@ export default class ProductListComponent implements OnInit {
     this._listFilter = value;
     this.filteredProducts = this.performFilter(value);
   }
+
+  ngOnInit(): void {
+    this.service = this.productsService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+  ngOnDestroy(): void {
+    this.service.unsubscribe();
+  }
+
   onChange(args: number) {
     console.log(args);
   }
@@ -51,9 +52,6 @@ export default class ProductListComponent implements OnInit {
   onRatingClick = (message: string): void => {
     this.message = message;
   };
-  ngOnInit(): void {
-    this.filteredProducts = this.performFilter('');
-  }
   performFilter(value: string): IProduct[] {
     return this.products.filter((product) =>
       product.productName.toLowerCase().includes(value.toLowerCase())
