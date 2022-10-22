a4e221dc010efed5bd2a795b491e4ed4a3bbde73
PTAHume <patricktahume@gmail.com>
2022-10-22 18:50:30 +0100

    adding tabs

--- src/app/products/product-edit/product-edit-info.component.ts
+++ src/app/products/product-edit/product-edit-info.component.ts
@@ -5,16 +5,22 @@ import { NgForm } from '@angular/forms';
 import { Product } from '../product';
 
 @Component({
-  templateUrl: './product-edit-info.component.html'
+  templateUrl: './product-edit-info.component.html',
 })
 export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm) productForm?: NgForm;
 
   errorMessage = '';
-  product = { id: 1, productName: 'test', productCode: 'test', description: 'test' };
+  product: Product | null = null;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.parent?.data.subscribe((data) => {
+      this.product = data['resolvedData'].product;
+      if (this.productForm) {
+        this.productForm.reset();
+      }
+    });
   }
 }
