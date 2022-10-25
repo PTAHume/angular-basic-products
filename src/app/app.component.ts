c049662656ddd99703e85ffeac32d739ef93576d
PTAHume <patricktahume@gmail.com>
2022-10-25 12:45:49 +0100

    route wait animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel,
+} from '@angular/router';
 
+import { slideInAnimation } from './app.animation';
 import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation],
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading = true;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    } else if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationError ||
+      routerEvent instanceof NavigationCancel
+    ) {
+      this.loading = false;
+    }
+  }
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
